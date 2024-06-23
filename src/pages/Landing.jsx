84db01ffa4195a1b3778af59fa746979ba5da4fe
mojeda20 <130985132+bastidasgiampiero@users.noreply.github.com>
2024-06-23 13:22:20 -0400

    Start Profil Page :star2:

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { logout } from "../controllers/auth";
 import { UserContext } from "../hooks/user";
 import { useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
+import HeaderSesion from "../components/HeaderSesion";
 
 const Landing = () => {
   const { setUser } = useContext(UserContext);
@@ -14,9 +16,13 @@ const Landing = () => {
   };
 
   return (
-    <div>
-      <h1>Hola Bienvenidos</h1>
-      <button onClick={handleLogout}>Logout</button>
+    <div className="flex flex-col min-h-screen bg-orange-600">
+      <HeaderSesion />
+      <main className="flex-grow flex justify-center items-center p-5 box-border mt-24">
+        <h1>Hola Bienvenidos</h1>
+        <button onClick={handleLogout}>Logout</button>
+      </main>
+      <Footer />
     </div>
   );
 };
