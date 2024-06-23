137fd27c1e0c5d97fa1131c41ad2a89ff0802361
mojeda20 <122550998+mafersanchezp@users.noreply.github.com>
2024-06-23 14:15:41 -0400

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,7 +7,7 @@ const Register = () => {
   return (
     <div className="flex flex-col min-h-screen bg-orange-600">
       <Header />
-      <main className="flex-grow flex justify-center items-center p-5 box-border mt-24">
+      <main className="flex-grow flex justify-center items-center p-6 box-border mt-24">
         <RegisterForm />
       </main>
       <Footer />
